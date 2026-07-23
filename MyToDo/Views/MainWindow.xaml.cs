6241d8120a1423f484dde2ff0a3a34bed87df6b8
86183 <86183@DESKTOP-OSELEKV>
2026-07-23 17:10:09 +0800

    添加项目文件。

---
+++ MyToDo/Views/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace MyToDo.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            btnMin.Click += (s, e) =>
+            {
+                this.WindowState = WindowState.Minimized;
+            };
+            btnMax.Click += (s, e) =>
+            {
+                if (this.WindowState == WindowState.Maximized)
+                    this.WindowState = WindowState.Normal;
+                else
+                    this.WindowState = WindowState.Maximized;
+            };
+            btnClose.Click += (s, e) =>
+            {
+                this.Close();
+            };
+
+            ColorZone.MouseMove += (s, e) =>
+            {
+                if (e.LeftButton == MouseButtonState.Pressed)
+                    this.DragMove();
+            };
+
+            ColorZone.MouseDoubleClick += (s, e) =>
+            {
+                if (this.WindowState == WindowState.Normal)
+                    this.WindowState = WindowState.Maximized;
+                else
+                    this.WindowState = WindowState.Normal;
+            };
+        }
+    }
+}
